e97262c3ff3e521fe29d34ba684785f4b4371e60
nazmul_khan <nazmul.khan121@gmail.com>
2026-06-09 21:48:38 +0600

    problem number 08 is done

--- script2.js
+++ script2.js
@@ -31,3 +31,24 @@ function countVowels(str) {
   return count;
 }
 console.log(countVowels('Bangladesh is our homeland'));
+
+console.log(`======================= ### =======================`);
+
+// Problem 8: Check Palindrome  [Easy]
+// Description: Write a function isPalindrome(str) that returns true if the string reads the same forwards and backwards.
+// Example:
+// Input: 'racecar'  → Output: trueInput: 'hello'    → Output: false
+// Hint: Compare the string to its reverse.
+
+function isPalindrome(str) {
+  let rev = str.split('').reverse().join('');
+
+  if (rev == str) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isPalindrome('abba'));
+
+console.log(`======================= ### =======================`);
