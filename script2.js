498c144a92d40948dd0394e67cfbea57e165b2dd
nazmul_khan <nazmul.khan121@gmail.com>
2026-06-09 22:50:08 +0600

    solved the nineth problem

--- script2.js
+++ script2.js
@@ -52,3 +52,19 @@ function isPalindrome(str) {
 console.log(isPalindrome('abba'));
 
 console.log(`======================= ### =======================`);
+
+// Problem 9: Capitalize First Letter of Each Word  [Easy]
+// Description: Write a function titleCase(str) that capitalizes the first letter of every word in a string.
+// Example:
+// Input: 'hello world'  → Output: 'Hello World'
+// Hint: Use split(' '), map(), and join(' ').
+
+function titleCase(str) {
+  return str
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+console.log(titleCase('hello world'));
+
+console.log(`======================= ### =======================`);
