5390bba6c593a85e2ad4552db71e5a8c576acd65
nazmul_khan <nazmul.khan121@gmail.com>
2026-06-11 19:50:45 +0600

    solved the problem number ten

--- script2.js
+++ script2.js
@@ -68,3 +68,21 @@ function titleCase(str) {
 console.log(titleCase('hello world'));
 
 console.log(`======================= ### =======================`);
+
+// Problem 10: Count Occurrences of a Character  [Easy]
+// Description: Write a function countChar(str, char) that returns how many times a character appears in a string.
+// Example:
+// Input: 'banana', 'a'  → Output: 3
+// Hint: Use split(char).length - 1 or a loop.
+
+function countChar(str, char) {
+  let count = 0;
+
+  for (const currentNumber of str) {
+    if (currentNumber === char) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countChar('Nazmul is a developer', 'a'));
