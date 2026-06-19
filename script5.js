e9275f40c441f3258cf4b1a731e27f15116761fd
nazmul_khan <nazmul.khan121@gmail.com>
2026-06-19 08:57:09 +0600

    create a curry function

--- script5.js
+++ script5.js
@@ -64,3 +64,21 @@ c.increment();
 c.increment();
 
 console.log(c.getCount());
+
+console.log(`========================= ### =========================`);
+
+// Problem 24: Curry a Function  [Medium]
+// Description: Write a function curry(fn) that converts a function of two arguments into a curried version.
+// Example:
+// const add = curry((a,b) => a+b);add(2)(3); // 5
+// Hint: Return a function from inside a function.
+
+const curry = (fn) => {
+  return function (a) {
+    return function (b) {
+      return fn(a, b);
+    };
+  };
+};
+const add = curry((a, b) => a + b);
+console.log(add(4)(8));
