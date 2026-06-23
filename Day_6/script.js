a1a0cc9acbc8241dd9e6dc7ba0f74aded72b2978
nazmul_khan <nazmul.khan121@gmail.com>
2026-06-23 08:48:16 +0600

    create a spad operate when merge multiple arrays

--- Day_6/script.js
+++ Day_6/script.js
@@ -23,3 +23,17 @@ const user = {
 const { firstName: name, lastName, age } = user;
 
 console.log(`My Name is ${name} ${lastName} & age is ${age}`);
+
+console.log(`============================ ### ============================`);
+
+// Problem 27: Merge Arrays with Spread  [Easy]
+// Description: Write a function mergeArrays(...arrays) that accepts any number of arrays and merges them into one using the spread operator.
+// Example:
+// Input: [1,2], [3,4], [5]Output: [1,2,3,4,5]
+// Hint: Use rest parameters and spread inside reduce or flat.
+
+const mergeArrays = (...arrays) => {
+  return arrays.flat();
+};
+
+console.log(mergeArrays([1, 2], [3, 4]));
